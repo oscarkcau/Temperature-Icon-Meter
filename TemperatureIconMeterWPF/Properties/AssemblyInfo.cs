c012eb2c6b19fc94ea438fb2912a94ca0f0c22be
Oscar Kin-Chung Au <oscarkcau@gmail.com>
2025-03-16 05:07:35 +0800

    Update 3rd party libraries. Update version number.

--- TemperatureIconMeterWPF/Properties/AssemblyInfo.cs
+++ TemperatureIconMeterWPF/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Temperature Icon Meter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Small notifyicon system temperature meter")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Maybe Useless Software")]
 [assembly: AssemblyProduct("Temperature Icon Meter")]
-[assembly: AssemblyCopyright("Copyright ©  2022 Oscar Au")]
+[assembly: AssemblyCopyright("Copyright ©  2025 Oscar Au")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.3.0")]
-[assembly: AssemblyFileVersion("2.3.3.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
